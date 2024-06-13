117536ba35daeeaeb9145897b2fc759188112169
santiocampo1 <sanntiocampo@gmail.com>
2024-06-13 10:56:24 -0300

    base html and posts

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Destiny Franks',
+        'title': 'Blog Post 1',
+        'content': 'This is my first blog post',
+        'date_posted': '12th June, 2024',
+    },
+
+    {
+        'author': 'Santiago Ocampo',
+        'title': 'Blog Post 2',
+        'content': 'This is my second blog post',
+        'date_posted': '13th June, 2024',
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About Page'})
