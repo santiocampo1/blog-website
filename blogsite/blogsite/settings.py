20afc3403c9eab86bc78c7d491930da9fccc1f14
santiocampo1 <sanntiocampo@gmail.com>
2024-06-24 12:46:10 -0300

    Post method

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     # by my own
     'blog.apps.BlogConfig',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
